33b764a32d42eba1e3fc7c82848da6a0dc7a255e
IndoPhoenix <vigyanavart@gmail.com>
2025-05-18 11:05:51 +0530

    OTP Component

---
+++ Otp.jsx
@@ -0,0 +1,76 @@
+import {useRef, useState, createRef} from "react";
+export default function OTP(
+    {number}
+){
+
+const [otparray, setOtparray] = useState(Array(number).fill(""));
+    const refs = useRef(Array.from({length : number}, ()=>createRef()));
+
+
+return (
+    <div className = 'flex'>
+    {
+        Array(number).fill(1).map((val,index)=>{
+           return (<OtpBox key={index} index = {index} otparray = {otparray} setOtparray = {setOtparray}reference = {refs.current[index]} goNext = {()=>{
+            if(index != refs.current.length-1){
+                refs.current[index+1].current.focus();
+            }
+           }} goBack = {()=>{
+            if(index != 0){
+                refs.current[index-1].current.focus();
+            }
+           }}></OtpBox>)
+        })
+    }
+    </div>
+)
+
+
+
+
+}
+
+function OtpBox({reference, goNext, goBack, index, otparray, setOtparray}){
+    const [val,setVal] = useState("");
+    const preval = otparray[index];
+    let newotparray = [...otparray];
+    
+    return (
+        <div className="m-1 rounded-full border-2 outline-none border-solid border-red-500">
+        <input className = "rounded-full outline-none px-4 w-[40px] h-[50px]" value = {preval?preval:val} ref = {reference} type = "text" 
+        
+        onPaste={e=>{
+            const data = e.clipboardData.getData("text");
+            const chars = data.split("");
+            let newotparray = [...otparray];
+            
+            for(let i = 0; i< chars.length; i++){
+                if(parseInt(chars[i]) >= 0 && parseInt(chars[i]) < 10){
+                    newotparray[i+index] = chars[i];
+                }
+               
+            }
+            setOtparray(newotparray);
+            
+
+        }}
+        onKeyDown= {(e)=>{
+            if(e.key == 'Backspace'){
+                // console.log("changed bhai")
+                newotparray[index] = "";
+                setOtparray(newotparray);
+                setVal("");
+                goBack();
+            }
+        }}onChange={(e)=>{
+            const str = e.target.value[e.target.value.length -1];
+            if(parseInt(str)>=0 && parseInt(str)<10){
+                newotparray[index] = str;
+                setOtparray(newotparray);
+                setVal(str);
+                goNext();
+            }
+        }}></input>
+        </div> 
+    )
+}
